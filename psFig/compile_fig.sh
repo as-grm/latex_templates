aa8604bd2159e958ee924fcc22354d9d316eec4d
Aleksander GRM <aleksander.grm@fpp.uni-lj.si>
2024-12-13 14:11:36 +0100

    Update

--- psFig/compile_fig.sh
+++ psFig/compile_fig.sh
@@ -4,7 +4,7 @@ file_name=$1
 echo "Compile psTricks file $file_name ..."
 
 latex $file_name.tex
-dvips $file_name.dvips
+dvips $file_name.dvi
 ps2pdf -dALLOWPSTRANSPARENCY $file_name.ps
 
 rm -rf *.aux *.dvi *.log *.ps
